85004922ccbd358657986534feb66c9afae59962
smfulton <sfulton1996@gmail.com>
2024-09-06 16:55:10 -0500

    Bonus was broken because I can't read. Corrected with input.next() pulled from code posted by Ariel

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,16 +6,17 @@ public class Area {
     public static void main(String[] args){
         double radius;
         Scanner input = new Scanner(System.in);
-        do{
-            System.out.println("Enter the radius of your circle:");
-            radius = input.nextDouble();
-            if(radius < 0.0){
-                System.out.println("Enter a positive number.");
+        do {
+            System.out.println("Enter a positive number for the radius: ");
+            while (!input.hasNextDouble()) {
+                System.out.println("Radius must be a number - try again!");
+                input.next();
             }
-        }while(radius < 0.0);
-
+            radius = input.nextDouble();
+        } while (radius <= 0);
+        input.close();
         double area = Circle.getArea(radius);
-        System.out.printf("The area of a circle of radius %s is %s%n",radius,area);
+        System.out.printf("The area of a circle of radius %s is: %s%n", radius, area);
     }
 }
 
